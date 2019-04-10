dc3b78f82b6f5765161e2a8b5e3a2d619b3bc4d1
visualstudio <visualstudio@122-PC27>
2019-04-10 10:43:35 -0400

    Section A

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,15 +1,44 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+
 
 namespace ConsoleApp1
 {
     class Program
     {
+        ArrayList Beowulf;
         static void Main(string[] args)
         {
+            Program a = new Program();
+            a.Beowulf = new ArrayList();
+            a.ReadTextFiles();
+        }
+
+        public void Run() { this.ReadTextFiles(); }
+        public void ReadTextFiles()
+        {
+
+            using (StreamReader sr = new StreamReader("U:/Users/727454/beowulf.txt"))
+            {
+                string line;
+                int counter = 0;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                    Beowulf.Add(line);
+                    counter++;
+                }
+
+                Console.WriteLine("Lines:" + counter);
+                Console.ReadLine();
+            }
+
         }
     }
 }
